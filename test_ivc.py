c670a9acff0c356ae98b804bdd70d074749923e2
Alexander Mackay <a.mackay@nchain.com>
2020-02-29 18:51:50 +0000

    Change test parameters to more representative values.

--- test_ivc.py
+++ test_ivc.py
@@ -1,20 +1,18 @@
-# This programme calculates the implied volatility for....
+"""
+Mako Interview Question.
 
-# Interview question for Mako
+This performs unit tests for ivc.py
 
-# Requires: Python 3.6, Pandas, numpy
+# Requirements: Python 3.6 (or later), unittest, ivc
+
+# Inputs for functions have been randomly chosen...
+
+"""
 
 import unittest
 import numpy as np
 import ivc
 
-"""
-print("Black Scholes Call:", ivc.BSC_call(100, 90, 0.01, 0.1, 0.1))
-print("Black Scholes Put:", ivc.BSC_put(90, 100, 0.01, 0.1, 0.1))
-print("Bachelier Call", ivc.BAC_call(100, 90, 0.01, 0.1, 0.1))
-print("Bachelier Put", ivc.BAC_put(90, 100, 0.01, 0.1, 0.1))
-quit()
-"""
 
 # Example functions for testing Newton solver
 def g(S, K, r, x, T):
@@ -29,46 +27,48 @@ class Testivc(unittest.TestCase):
     def test_BSC(self):
         # Test Black Scholes model functions
         r, sig, T = 0.01, 0.1, 0.01 # Constant risk-free, volatility, expiry date
-        S, K  = 100, 90 # Call inputs
-        call_ans = 10.090254370495515
+        S, K  = 1.1, 0.9 # Call inputs
+        call_ans = 0.20008999550015005
         self.assertAlmostEqual(ivc.BSC_call(S, K, r, sig, T), call_ans)
-        S, K = 90, 100 # Put inputs
-        put_ans = 9.900431312968252
+        S, K = K, S # Put inputs
+        put_ans = 0.19989000549981684
         self.assertAlmostEqual(ivc.BSC_put(S, K, r, sig, T), put_ans)
         
 
     def test_BAC(self):
         # Test Bachelier model functions
         r, sig, T = 0.01, 0.1, 0.01
-        S, K = 100, 90              # Call inputs
-        call_ans = 9.99000499833375
+        S, K = 1.1, 0.9              # Call inputs
+        call_ans = 0.19998000099996674
         self.assertAlmostEqual(ivc.BAC_call(S, K, r, sig, T), call_ans)
 
-        S, K = 90, 100              # Put inputs
-        call_ans = 9.99000499833375
+        S, K = K, S              # Put inputs
+        put_ans = 0.19998000099996674
         self.assertAlmostEqual(ivc.BAC_put(S, K, r, sig, T), put_ans)
 
 
-    def test_BSC_imvol(self):
+    def test_BSCvol(self):
         # Test Black Scholes call option IV
-        iv_actual = 0.1
+        iv_actual = 0.5
         r, T = 0.01, 0.01
-        call_ans, S, K = 10.090254370495515, 100, 90 
-        self.assertAlmostEqual(ivc.BAC_imvol(call_ans, S, K, r, T, 'Call'), iv_actual)
+        call_ans, S, K = 0.10038900828135255, 1.0, 0.9 
+        self.assertAlmostEqual(ivc.imvol('Call', 'BlackScholes', call_ans, S, K, r, T), iv_actual)
         
         # Test Black Scholes put option IV
-        put_ans, S, K = 9.900431312968252, 90, 100
-        self.assertAlmostEqual(ivc.BAC_imvol(put_ans, S, K, r, T, 'Put'), iv_actual)
+        put_ans, S, K = 0.10020234665104932, 0.9, 1.0
+        self.assertAlmostEqual(ivc.imvol('Put', 'BlackScholes', put_ans, S, K, r, T), iv_actual)
 
-    def test_BAC_imvol(self):
+    def test_BACvol(self):
         # Test Bachelier  call option IV
-        iv_actual = 0.1
-        r, T = 0.01, 0.01
-        call_ans, S, K = 9.99000499833375, 100, 90
-        self.assertAlmostEqual(ivc.BAC_imvol(call_ans, S, K, r, T, 'Call'), iv_actual)
+        iv_actual = 0.5
+        r, T = 0.01, 0.1
+        call_ans, S, K = 0.12516446483551702, 1.0, 0.9
+        self.assertAlmostEqual(ivc.imvol('Call', 'Bachelier', call_ans, S, K, r, T), iv_actual)
+
         # Test Bachelier put option IV
-        put_ans, S, K = 9.99000499833375, 90, 100       
-        self.assertAlmostEqual(ivc.BAC_imvol(put_ans, S, K, r, T, 'Put'), iv_actual)
+        put_ans, S, K = 0.12516446483551702, 0.90, 1.0  
+        f, df = ivc.BAC_put, ivc.BAC_put_dsig     
+        self.assertAlmostEqual(ivc.imvol('Put', 'Bachelier', put_ans, S, K, r, T), iv_actual)
 
     def test_newton(self):
         """
@@ -76,13 +76,12 @@ class Testivc(unittest.TestCase):
         g is the (transcendental) function defined above
         """   
         ans = 3.6095232769758385 # Wolfram Alpha 
-        self.assertAlmostEqual(ivc.newton(g, g_dash, 250, 100, 100, 1, 1), ans)
-
+        x0 = 10
+        self.assertAlmostEqual(ivc.newton(g, g_dash, x0, 250, 100, 100, 1, 1), ans)
 
 
 if __name__ == 'main':
     unittest.main()
- 
 
 
 
